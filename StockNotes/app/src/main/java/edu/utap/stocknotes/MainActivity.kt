f64cc366c1e51cef270ce4529ff0e2962248cfc4
JarJarBB <philippe897@gmail.com>
2020-11-03 20:47:18 +0100

    Just getting started.

---
+++ StockNotes/app/src/main/java/edu/utap/stocknotes/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.utap.stocknotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
