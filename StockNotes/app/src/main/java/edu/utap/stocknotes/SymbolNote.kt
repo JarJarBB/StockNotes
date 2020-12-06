5e88b38db624fb20a4b37a6e18c9e474ee1a4f10
Mayank <mayank@endovisionai.com>
2020-12-07 05:11:27 +0530

    adding save

--- StockNotes/app/src/main/java/edu/utap/stocknotes/SymbolNote.kt
+++ StockNotes/app/src/main/java/edu/utap/stocknotes/SymbolNote.kt
@@ -2,7 +2,7 @@ package edu.utap.stocknotes
 
 data class SymbolNote (
     val symbol: String? = null,
-    val note: String? = null,
+    var note: String? = null,
     val name: String? = null,
     val deleted : String?=null
 
